8ce6080fd74d12ac7313361afdd95bf163b7893a
Kurt Beheydt <kurt@postduif.be>
2024-05-11 15:40:12 +0200

    three leds

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,35 @@
 #include <Arduino.h>
 
-#define pinLed1 5
+struct Led {
+    const uint8_t ledPin;
+    bool status;
+};
+
+const uint8_t ledCount = 3;
+
+Led leds[ledCount] = {
+    {5, false},
+    {4, false},
+    {14, false}};
 
 void setup() {
     Serial.begin(115200);
     Serial.println();
 
-    pinMode(pinLed1, OUTPUT);
+    for (uint8_t i = 0; i < ledCount; i++) {
+        pinMode(leds[i].ledPin, OUTPUT);
+    }
 }
 
 void loop() {
-    digitalWrite(pinLed1, HIGH);
-    Serial.println("led on");
-    delay(500);
-    digitalWrite(pinLed1, LOW);
-    Serial.println("led off");
-    delay(500);
+    for (uint8_t i = 0; i < ledCount; i++) {
+        digitalWrite(leds[i].ledPin, HIGH);
+        Serial.print("led ");
+        Serial.print(i);
+        Serial.print(" on");
+        delay(200);
+        digitalWrite(leds[i].ledPin, LOW);
+        Serial.println("... off");
+        delay(200);
+    }
 }
